ac2f35019637d068f80abb8068838814c0696dc6
Mark Stephen <mark.zizon@gmail.com>
2023-08-01 16:48:26 +0800

    ui/projects

--- js/index.js
+++ js/index.js
@@ -3,3 +3,13 @@ AOS.init();
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
+
+window.addEventListener('load', function() {
+    const projects = this.document.querySelectorAll('[data-link]');
+    projects.forEach(project => {
+        console.log(project)
+        project.addEventListener('click', () => {
+            window.open(project.getAttribute('data-link'), '_blank')
+        });
+    })
+});
