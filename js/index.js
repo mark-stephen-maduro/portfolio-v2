bcb5f9bf242318ab8029f43cc5bbe343cd46b781
Stephen Maduro <smaduro@owc.com>
2023-08-02 16:12:50 +0800

    projects slider

--- js/index.js
+++ js/index.js
@@ -4,12 +4,28 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     const projects = this.document.querySelectorAll('[data-link]');
     projects.forEach(project => {
-        console.log(project)
         project.addEventListener('click', () => {
             window.open(project.getAttribute('data-link'), '_blank')
         });
-    })
+    });
+
+    new Swiper('.swiper', { // Optional parameters
+        slidesPerView: 7,
+        loop: true,
+        spaceBetween: 10,
+        autoplay: true
+    });
+
+    new Swiper('.swiper-projects', { // Optional parameters
+        slidesPerView: 2,
+        spaceBetween: 10,
+        autoplay: {
+            delay: 3000
+        },
+        speed: 500,
+        loop: true
+    });
 });
